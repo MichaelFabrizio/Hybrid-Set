465efed31c081754f7def86451c2e2749d58d5d4
mikef <mikefabrizio94@gmail.com>
2023-10-15 10:29:05 -0400

    Branch testing

--- include/Hybrid_Set.h
+++ include/Hybrid_Set.h
@@ -1,28 +1,14 @@
 #pragma once
 #include <iostream>
-#include <tuple>
 
 template <typename T, std::size_t N>
 class Hybrid_Set
 {
   typedef std::size_t Entity;
   typedef std::size_t Index;
-  typedef std::tuple<Index, Index, Index> Location_Set;
 
   inline bool ID_Exists_On_Left(const Entity& EntityID) { 
     return (EntityID < Next_Empty) ? true : false;
-  }
-
-  inline Location_Set Find_Locations(const Entity& EntityID)
-  {
-    Location_Set locations;
-    
-    locations = { EntityID & Old_Bitmask,
-                  EntityID & Level_Bitmask,
-                  S[EntityID]
-    };
-
-    return locations;
   }
 
   inline Index Find_Exact_Location(const Entity& ID)
@@ -30,7 +16,7 @@ class Hybrid_Set
     Index INDEX_0 = ID & Old_Bitmask;
     Index INDEX_1 = ID & Level_Bitmask;
 
-    if        (ID_Exists_On_Left(ID))   { return ID;      }
+    if        (ID_Exists_On_Left(ID) && S[ID] == ID)   { return ID; }
     else if   (S[INDEX_0] == ID)        { return INDEX_0; }
     else if   (S[INDEX_1] == ID)        { return INDEX_1; }
 
@@ -41,56 +27,98 @@ class Hybrid_Set
     else                                { return 0; }       // Final case: Not found
   }
 
-  inline Entity Place_R1(const Entity& ID)
+  inline void Place_R0(const Entity ID, const bool Direction)
+  {
+    if (Direction) { 
+    std::cout << "ID: " << ID << '\n';
+    std::cout << "Left Place" << '\n';
+      const auto ID_To_Swap = S[ID];
+      S[ID] = ID;
+      
+      bool ID_To_Swap_Direction = ID_Exists_On_Left(ID_To_Swap);
+      Place_R0(ID_To_Swap, ID_To_Swap_Direction);
+    }
+    else if (ID == Next_Empty)
+    { Place_R2(ID); }
+    else
+    {
+      Place_R1(ID);
+    }
+  }
+
+  inline void Place_R1(const Entity ID)
   {
     const auto Candidate_Index = ID & Level_Bitmask;
 
-    if (Candidate_Index < Next_Empty)
-    { 
-      const auto Previous_ID = S[Candidate_Index];
-      if (Previous_ID < ID) { return ID; }
-      
-      else {
-        S[Candidate_Index] = ID;
-        auto Next_ID = Place_R1(Previous_ID);
-        return Next_ID; }
+    if (Candidate_Index >= Next_Empty)
+    {
+      Place_R2(ID);
+      return;
     }
-    return ID;
+
+    const auto Candidate_Owner = S[Candidate_Index];
+    if (Candidate_Owner < ID)
+    {
+      Place_R2(ID);
+    }
+    else
+    {
+      std::cout << "ID: " << ID << '\n';
+      std::cout << "Masked place: " << Candidate_Index << '\n';
+      S[Candidate_Index] = ID;
+      Place_R2(Candidate_Owner); //Optional Feature: Call Place_R1 here instead
+    }
   }
 
-  inline void Place_R2(const Entity& ID)
+  inline void Place_R2(const Entity ID)
   {
-    S[Next_Empty] = ID;
-    S[ID] = Next_Empty;
+    
+    std::cout << "ID: " << ID << '\n';
+    const auto Next_Empty_ID_Pointer = S[Next_Empty];
+    const auto Next_Empty_Bitmask_Pointer = Next_Empty & Old_Bitmask;
+    const auto Next_Empty_Bitmask_Value = S[Next_Empty_Bitmask_Pointer];
+    
+    if (Next_Empty_ID_Pointer != 0)
+    {
+      if (Next_Empty == ID) { std::cout << "Already exists!\n"; return; }
+      S[Next_Empty] = Next_Empty;
+      S[Next_Empty_ID_Pointer] = ID;
+      S[ID] = Next_Empty_ID_Pointer;
+    std::cout << "Endpoint Swap and place: " << Next_Empty_ID_Pointer << '\n';
+    }
+    else if (Next_Empty_Bitmask_Value == Next_Empty)
+    {
+      S[Next_Empty] = Next_Empty;
+      S[Next_Empty_Bitmask_Pointer] = ID;
+      S[ID] = Next_Empty_Bitmask_Pointer;
+    std::cout << "Endpoint Bitmask-Swap and place: " << Next_Empty_Bitmask_Pointer << '\n';
+    }
+    else
+    {
+      if (Next_Empty == ID)
+      {
+    std::cout << "Endpoint Exact place: " << Next_Empty << '\n';
+        S[Next_Empty] = ID;
+      }
+      else
+      {
+    std::cout << "Endpoint Index place: " << Next_Empty << '\n';
+        S[Next_Empty] = ID;
+        S[ID] = Next_Empty;
+      }
+    }
+    
     Count++;
     Next_Empty++;
-    Internal_Update_Bitmasks(); // Must be called after Count++ and Next_Empty++. A given Set, H, needs updated leading bit & associative masks internally.
-  }
-
-  template <std::size_t R = 0>
-  inline void Place(const Entity& ID)
-  {
-    if constexpr (R == 0) {
-      if (ID_Exists_On_Left(ID)) {        // Add a limit on this recursion branch. TODO: Refactor into a Conditional_Place_Lefthanded method.
-        const auto Previous_ID = S[ID];
-        S[ID] = ID;
-        Place<R>(Previous_ID); }
-      
-      else { Place<R+1>(ID); } }
-
-    if constexpr(R == 1) { const auto Next_ID = Place_R1(ID); Place<R+1>(Next_ID); }
-    if constexpr(R == 2) { Place_R2(ID); }
+    Internal_Update_Bitmasks();
   }
 
   inline void Internal_Update_Bitmasks()
   {
-    // checks if the next empty square in our array has reached a new order of magnitude
     std::size_t Next_Level_Bool = ((Next_Empty) & Get_Next_Level) >> Level;
 
-    //temporary: ensure output is always 1u or 0u, (output must always be 1u or 0u)
     if (Next_Level_Bool > 1u) { throw "next_level_bool weird output"; }
 
-    //updates bitmask and level data
     else if (Next_Level_Bool)     {
       Old_Bitmask = Level_Bitmask;
       Level++;
@@ -109,50 +137,30 @@ class Hybrid_Set
 
     void Add(const Entity& ID) 
     { 
+      bool direction = ID_Exists_On_Left(ID);
+
       if (ID == 0) { return; } //TODO: Return 0
       if (ID > 0xFFFF) { std::cout << "Underflow ID detected\n"; return; } //Extra checks for testing purposes; Also return 0
       if (ID >= N) { std::cout << "Overflow ID detected\n"; return; } //return 0
-      if (Next_Empty == N) { throw "[Add Function]: Error - Array bounds were allowed to overflow\n"; }
-      Place<0>(ID);
+      if (Next_Empty == N) { throw "Array bounds overflow detected\n"; }
+      
+      Place_R0(ID, direction);
 
       //TEMPORARY LOGIC
        Index Result = Find_Exact_Location(ID);
+
        if (Result == 0) { throw "[Add Function]: Result shouldn't be zero anymore!\n"; }
-       if (Result > (N-1)) { throw "[Add Function]: Critical index overflow error.}
+       if (Result > (N-1)) { throw "[Add Function]: Critical index overflow error.\n"; }
       //END TEMP LOGIC
 
       //Will return T[Result]
     }
 
-    void Remove(const Entity& ID) //Could it make sense to be passing ID by value for these?
-    {
-      const auto Last_ID = S[Count];
-      if (ID_Exists_On_Left(ID) && S[ID] == ID) //Found ID to remove in contiguous section (Left side)
-      {
-        if (Last_ID != Count)           //Case 1: S[Count] is indexed (Defined as Exists_On_Right() = True)
-        { 
-          S[Count] = 0;
-          S[ID] = Last_ID;
-          S[Last_ID] = ID;
-        }
-        else //Last_ID == Count        //Case 2: S[Count] = Count. Only occurs on an exact match, and requires different remove logic.
-        {
-          S[Count] = ID;
-          S[ID] = Last_ID;
-        }
-
-        Count--;
-        Next_Empty--;
-      }
-      else
-      {
-        const auto ID_To_Remove_Index = Find_Exact_Location(ID);
-        throw "Not yet implemented\n";
-      }
-    }
+    void Remove(const Entity& ID) { throw "Not yet implemented\n"; }
 
     void Debug() 
     { 
+      std::cout << "Index variables start at: " << Next_Empty << '\n';
       for (std::size_t i=0; i!=N; i++)
       {
         std::cout << "Index: " << i << " Element: " << S[i] << '\n';
@@ -166,7 +174,7 @@ class Hybrid_Set
     std::size_t Count = 0u;               //Count represents the number of entities
     std::size_t Next_Empty = Count + 1u;  //Count + 1u represents the next "empty" element in our set.
     
-    std::size_t Level = 4u;                              
+    std::size_t Level = 2u;                              
     std::size_t Get_Next_Level = 1u << Level;           
     std::size_t Get_Current_Level = 1u << (Level - 1u);
     std::size_t Level_Bitmask = Get_Next_Level - 1u;
