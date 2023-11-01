11ce9a700d3a23bd86f3b13e9fd0388a3069b6eb
mikef <mikefabrizio94@gmail.com>
2023-11-01 10:40:00 -0400

    Fixes

--- include/Hybrid_Set.h
+++ include/Hybrid_Set.h
@@ -13,6 +13,12 @@ class Hybrid_Set
     return (EntityID < Next_Empty) ? true : false;
   }
 
+  // Add optional feature: If entity is found at Index_0 (old bitmask location),
+  // Index it right away! Use an overloaded FN of Find_Exact_Location().
+  // The overloaded FN should look up Index_2 first, then Index_1, then Index_0.
+  // This will "maybe" produce faster repeat Look Ups (needs benchmarking)
+  // TODO: Test the speed for different sized containers
+
   inline Index Find_Exact_Location(const Entity& ID)
   {
     Index INDEX_0 = ID & Old_Bitmask;
@@ -42,10 +48,9 @@ class Hybrid_Set
     }
     else if (ID == Next_Empty)
     { Place_R2(ID); }
+
     else
-    {
-      Place_R1(ID);
-    }
+    { Place_R1(ID); }
   }
 
   inline void Place_R1(const Entity ID)
@@ -68,6 +73,7 @@ class Hybrid_Set
       // std::cout << "ID: " << ID << '\n';
       // std::cout << "Masked place: " << Candidate_Index << '\n';
       S[Candidate_Index] = ID;
+      S[ID] = 0;
       Place_R1(Candidate_Owner); //Optional Feature: Call Place_R2 here instead (w/ extra checks)
     }
   }
@@ -82,7 +88,8 @@ class Hybrid_Set
     
     if (Next_Empty_ID_Pointer != 0)
     {
-      S[Next_Empty] = ID;
+      S[Next_Empty] = Next_Empty;
+      S[Next_Empty_ID_Pointer] = ID;
       // std::cout << "Endpoint Exact place: " << ID << '\n';
       Count++;
       Next_Empty++;
@@ -118,7 +125,8 @@ class Hybrid_Set
     S[ID_Index] = Last_ID_Value;
     if (Last_ID_Value != Count)
     {
-      S[Last_ID_Value] = ID;
+      S[Last_ID_Value] = ID_Index;
+      S[ID] = 0u;
       S[Count] = 0u;
     }
     else { S[Count] = ID_Index; }
@@ -149,11 +157,10 @@ class Hybrid_Set
     if (Previous_Level_NOT_Reached > 1u) { throw "previous_level_bool weird output"; }
 
     else if (!Previous_Level_NOT_Reached)     {
-      Old_Bitmask = Level_Bitmask;
+      Level_Bitmask = Old_Bitmask;
       Level--;
       Get_Next_Level = 1u << Level;
       Get_Current_Level = 1u << (Level - 1u);
-      Level_Bitmask = Get_Next_Level - 1u;
     }
   }
 
@@ -168,21 +175,22 @@ class Hybrid_Set
     { 
       bool direction = ID_Exists_On_Left(ID);
 
-      if (ID == 0) { return; } //TODO: Return 0
+      if (ID == 0u) { return; } //TODO: Return 0
+      if (S[ID] == ID) { return; }
       if (ID > 0xFFFF) { std::cout << "Underflow ID detected\n"; return; } //Extra checks for testing purposes; Also return 0
       if (ID >= Max_Size) { std::cout << "Overflow ID detected\n"; return; } //return 0
-      if (Next_Empty == Max_Size) { throw "Array bounds overflow detected\n"; }
+      if (Next_Empty >= Max_Size) { return; }
       
       Place_R0(ID, direction);
 
       //TEMPORARY LOGIC
        Index Result = Find_Exact_Location(ID);
 
-       if (Result == 0) { throw "[Add Function]: Result shouldn't be zero anymore!\n"; }
-       if (Result > (Max_Size-1)) { throw "[Add Function]: Critical index overflow error.\n"; }
+       if (Result == 0u) { throw "[Add Function]: Result shouldn't be zero anymore!\n"; }
+       if (Result > (Max_Size-1u)) { throw "[Add Function]: Critical index overflow error.\n"; }
       //END TEMP LOGIC
 
-      //Will return T[Result]
+      //Will return Result
     }
 
     void Remove(const Entity& ID) 
@@ -190,15 +198,17 @@ class Hybrid_Set
       if (ID == 0u) { return; }
       auto Possible_Index = ID;
 
-      if (ID_Exists_On_Left(ID) && (S[ID] == ID)) { Swap_And_Remove_Last_Entity(ID, Possible_Index); }
+      if (ID_Exists_On_Left(ID) && (S[ID] == ID)) { Swap_And_Remove_Last_Entity(ID, Possible_Index); } //Should be refactored
       else
       {
         Possible_Index = Find_Exact_Location(ID); //Implicitly an exact match at this point (or the Null Entity 0)
-        if (Possible_Index == 0u) { std::cout << "ID not found\n"; return; }
+        if (Possible_Index == 0u) { std::cout << "ID " << ID << " not found\n"; return; }
 
         Swap_And_Remove_Last_Entity(ID, Possible_Index);
       }
     }
+
+    Entity Find(const Entity ID) const { return Find_Exact_Location(ID); }
 
     void Clear()
     {
@@ -215,17 +225,21 @@ class Hybrid_Set
       Old_Bitmask = Get_Current_Level - 1u;
     }
 
+    std::size_t GetCount()  { return Count; }
+    std::size_t GetNext()   { return Next_Empty; }
+    std::size_t GetIndexOwner(Index index) { return S[index]; }
+
     void Debug() 
     { 
       std::cout << "Index variables start at: " << Next_Empty << '\n';
-      for (std::size_t i=0; i!=Max_Size; i++)
+      for (std::size_t i=0u; i!=Max_Size; i++)
       {
         std::cout << "Index: " << i << " Element: " << S[i] << '\n';
       }
     }
 
   private:
-    // T Elements[N];                     //Contiguous array of objects
+    //T Elements[Max_Size];               //Contiguous array of objects
     std::size_t S[Max_Size];              //Sparse array of entity IDs
 
     std::size_t Count = 0u;               //Count represents the number of entities
